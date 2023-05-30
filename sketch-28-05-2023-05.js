dc65e8a4151478280e7ecd9fb34388b9c25ba68d
march213 <jane.molodetskaya@gmail.com>
2023-05-29 20:02:31 -0400

    add comments to osc sketch

--- sketch-28-05-2023-05.js
+++ sketch-28-05-2023-05.js
@@ -1,4 +1,5 @@
 
+// before that also need to run hydra-osc to talk with supercollider on the same port
 
 await loadScript("https://cdn.jsdelivr.net/gh/ojack/hydra-osc/lib/osc.min.js");
 _osc = new OSC()
@@ -6,6 +7,7 @@ _osc.open()
 _osc.on("*", (m) => { console.log(m.address, m.args)})
 
 hue = 0
+// "/a" =  a channel from supercollider
 _osc.on("/a", (m) => {
 	hue = m.args[0]
 })
